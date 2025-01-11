13883d8a98c2fd4a80bc60b517f2dbad6f4cd0ae
Aaron Murray <plugins@omv-extras.org>
2025-01-11 11:05:03 -0600

    add datatable allowing adding and editing zram directories
    Signed-off-by: Aaron Murray <plugins@omv-extras.org>

--- srv/salt/omv/deploy/flashmemory/default.sls
+++ srv/salt/omv/deploy/flashmemory/default.sls
@@ -15,8 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-{% set extradirs = salt['pillar.get']('default:OMV_FM_EXTRA_DIRS', '') %}
-{% set tabs = '\t\t' %}
+{% set config = salt['omv_conf.get']('conf.service.flashmemory') %}
 
 configure_rrd_dir:
   file.directory:
@@ -31,26 +30,11 @@ configure_samba_dir:
 configure_flashmemory:
   file.managed:
     - name: "/etc/ztab"
-    - contents: |
-        {{ pillar['headers']['auto_generated'] }}
-        {{ pillar['headers']['warning'] }}
-        # swap  alg      mem_limit  disk_size  swap_priority  page-cluster  swappiness
-        swap    lzo-rle  250M		    750M       75             0             150
-        # dir  alg      mem_limit  disk_size  target_dir                   bind_dir
-        dir    lzo-rle  250M       750M       /var/tmp                     /opt/zram/vartmp.bind
-        dir    lzo-rle  50M        150M       /var/lib/openmediavault/rrd  /opt/zram/rrd.bind
-        dir    lzo-rle  50M        150M       /var/lib/rrdcached/          /opt/zram/rrdcached.bind
-        dir    lzo-rle  50M        150M       /var/lib/monit               /opt/zram/monit.bind
-        dir    lzo-rle  50M        150M       /var/cache/samba             /opt/zram/samba.bind
-        # log	 alg      mem_limit  disk_size  target_dir                   bind_dir                  oldlog_dir
-        log    lzo-rle  50M        150M       /var/log                     /opt/zram/log.bind        /opt/zram/oldlog
-        {%- set count = 0 %}
-        {%- for path in extradirs.split(',') %}
-        {%- if path.strip() | length > 1 %}
-        {%- set count = count + 1 %}
-        dir    lzo-rle  50M        150M       {{ path.strip() }}           /opt/zram/extra_{{ count }}.bind
-        {%- endif %}
-        {%- endfor %}
+    - source:
+      - salt://{{ tpldir }}/etc-ztab.j2
+    - template: jinja
+    - context:
+        config: {{ config | json }}
     - user: root
     - group: root
     - mode: 644
---
+++ usr/share/openmediavault/confdb/create.d/conf.service.flashmemory.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+set -e
+
+. /usr/share/openmediavault/scripts/helper-functions
+
+SERVICE_NAME="flashmemory"
+XPATH="/config/services/${SERVICE_NAME}"
+
+if ! omv_config_exists "${XPATH}"; then
+  omv_config_add_node "/config/services" "${SERVICE_NAME}"
+  omv_config_add_node "${XPATH}" "dirs"
+fi
+
+if ! omv_config_exists "${XPATH}/dirs/dir[uuid='ce7325d6-d032-11ef-9402-fb9e0253d055']"; then
+  object="<uuid>ce7325d6-d032-11ef-9402-fb9e0253d055</uuid>"
+  object="${object}<enable>1</enable>"
+  object="${object}<ztype>swap</ztype>"
+  object="${object}<path></path>"
+  object="${object}<memlimit>250</memlimit>"
+  object="${object}<disksize>750</disksize>"
+  omv_config_add_node_data "${XPATH}/dirs" "dir" "${object}"
+fi
+if ! omv_config_exists "${XPATH}/dirs/dir[uuid='d1d72880-d032-11ef-b216-dfdc0cb5988a']"; then
+  object="<uuid>d1d72880-d032-11ef-b216-dfdc0cb5988a</uuid>"
+  object="${object}<enable>1</enable>"
+  object="${object}<ztype>dir</ztype>"
+  object="${object}<path>/var/tmp</path>"
+  object="${object}<memlimit>250</memlimit>"
+  object="${object}<disksize>750</disksize>"
+  omv_config_add_node_data "${XPATH}/dirs" "dir" "${object}"
+fi
+if ! omv_config_exists "${XPATH}/dirs/dir[uuid='d2363d02-d032-11ef-8ab1-f7ed0db98337']"; then
+  object="<uuid>d2363d02-d032-11ef-8ab1-f7ed0db98337</uuid>"
+  object="${object}<enable>1</enable>"
+  object="${object}<ztype>dir</ztype>"
+  object="${object}<path>/var/lib/openmediavault/rrd</path>"
+  object="${object}<memlimit>50</memlimit>"
+  object="${object}<disksize>150</disksize>"
+  omv_config_add_node_data "${XPATH}/dirs" "dir" "${object}"
+fi
+if ! omv_config_exists "${XPATH}/dirs/dir[uuid='d287f0de-d032-11ef-b584-370df3ff4bfa']"; then
+  object="<uuid>d287f0de-d032-11ef-b584-370df3ff4bfa</uuid>"
+  object="${object}<enable>1</enable>"
+  object="${object}<ztype>dir</ztype>"
+  object="${object}<path>/var/lib/rrdcached/</path>"
+  object="${object}<memlimit>50</memlimit>"
+  object="${object}<disksize>150</disksize>"
+  omv_config_add_node_data "${XPATH}/dirs" "dir" "${object}"
+fi
+if ! omv_config_exists "${XPATH}/dirs/dir[uuid='d2d9a01e-d032-11ef-908a-a3e0867c35a7']"; then
+  object="<uuid>d2d9a01e-d032-11ef-908a-a3e0867c35a7</uuid>"
+  object="${object}<enable>1</enable>"
+  object="${object}<ztype>dir</ztype>"
+  object="${object}<path>/var/lib/monit</path>"
+  object="${object}<memlimit>50</memlimit>"
+  object="${object}<disksize>150</disksize>"
+  omv_config_add_node_data "${XPATH}/dirs" "dir" "${object}"
+fi
+if ! omv_config_exists "${XPATH}/dirs/dir[uuid='d32a95aa-d032-11ef-afe1-efb016e5b099']"; then
+  object="<uuid>d32a95aa-d032-11ef-afe1-efb016e5b099</uuid>"
+  object="${object}<enable>1</enable>"
+  object="${object}<ztype>dir</ztype>"
+  object="${object}<path>/var/cache/samba</path>"
+  object="${object}<memlimit>50</memlimit>"
+  object="${object}<disksize>150</disksize>"
+  omv_config_add_node_data "${XPATH}/dirs" "dir" "${object}"
+fi
+if ! omv_config_exists "${XPATH}/dirs/dir[uuid='03d39020-d034-11ef-b5e5-8fdcc613b8b0']"; then
+  object="<uuid>03d39020-d034-11ef-b5e5-8fdcc613b8b0</uuid>"
+  object="${object}<enable>1</enable>"
+  object="${object}<ztype>log</ztype>"
+  object="${object}<path>/var/log</path>"
+  object="${object}<memlimit>50</memlimit>"
+  object="${object}<disksize>150</disksize>"
+  omv_config_add_node_data "${XPATH}/dirs" "dir" "${object}"
+fi
+
+exit 0
